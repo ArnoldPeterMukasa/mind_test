1eb81d79f8cba11900bd1be44ea58bf3194cfbe9
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2025-06-19 19:38:00 +0300

    changes

--- ap_backend.py
+++ ap_backend.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for 
+import os
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template ('index.html')
@@ -9,9 +11,14 @@ def index():
 def yes():
     return render_template('yes_page.html')
 
-@app.route('/no')
-def no():
-    return render_template('no_page.html')
+@app.route('/exit')
+def exit(): #will shutdown the flask development server
+    # return render_template('no_page.html')
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        return 'server is not running with the Werkzeug Server'
+    func()
+    return 'Server shutting down....'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
