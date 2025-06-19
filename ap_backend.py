72409993461f5976b8daeeaaa571b6bc985db104
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2025-06-19 10:14:17 +0300

    modified the .py backend file

--- ap_backend.py
+++ ap_backend.py
@@ -2,3 +2,9 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 @app.route('/')
+def index():
+    return render_template ('index.html')
+
+@app.route('/yes')
+def yes():
+    return render_template('yes_page.html')
